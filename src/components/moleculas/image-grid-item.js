b8976b84394dff5c80a95f5a9a3cc990bd173e24
kryptonumstudio <kryptonumstudio@gmail.com>
2023-01-11 17:02:18 +0100

    copy configuration

--- src/components/moleculas/image-grid-item.js
+++ src/components/moleculas/image-grid-item.js
@@ -1,58 +1,121 @@
 import { GatsbyImage } from "gatsby-plugin-image"
-import React from "react"
+import React, { useRef } from "react"
+import { toast } from "react-toastify"
 import styled from "styled-components"
 import AddToFauvorite from "../atoms/add-to-favourite"
 
-export const ImageGridItem = ({ setRerender, rerender, image, popupNames }) => (
-    <Item>
-        <AddToFauvorite setRerender={setRerender} rerender={rerender} type={'product'} title={popupNames.model} />
-        <GatsbyImage className="image" image={image.localFile.childImageSharp.gatsbyImageData} alt={image.altText} />
-        <Data>
-            <span><span>Model:</span> <strong>{popupNames.model}</strong></span>
+const copySuccessed = {
+    en: 'Configuration copied successfully'
+}
 
-            {popupNames.material
-                ? <span><span>Material:</span> <strong>{popupNames.material}</strong></span>
-                : null}
+export const ImageGridItem = ({ setRerender, rerender, image, popupNames }) => {
 
-            {popupNames.tableTopMaterial
-                ? <span><span>Table top material:</span> <strong>{popupNames.tableTopMaterial}</strong></span>
-                : null}
+    const data = useRef(null)
 
-            {popupNames.leather
-                ? <span><span>Leather:</span> <strong>{popupNames.leather}</strong></span>
-                : null}
+    return (
+        <Item>
+            <AddToFauvorite setRerender={setRerender} rerender={rerender} type={'product'} title={popupNames.model} />
+            <GatsbyImage className="image" image={image.localFile.childImageSharp.gatsbyImageData} alt={image.altText} />
+            <Flex>
+                <Data ref={data}>
+                    <span><span>Model:</span> <strong>{popupNames.model}</strong></span>
 
-            {popupNames.fabric
-                ? <span><span>Fabric:</span> <strong>{popupNames.fabric}</strong></span>
-                : null}
+                    {popupNames.material
+                        ? <span><span>Material:</span> <strong>{popupNames.material}</strong></span>
+                        : null}
 
-            {popupNames.legs
-                ? <span><span>Legs:</span> <strong>{popupNames.legs}</strong></span>
-                : null}
+                    {popupNames.tableTopMaterial
+                        ? <span><span>Table top material:</span> <strong>{popupNames.tableTopMaterial}</strong></span>
+                        : null}
 
-            {popupNames.materialOfTheLegs
-                ? <span><span>Material of the legs:</span> <strong>{popupNames.materialOfTheLegs}</strong></span>
-                : null}
+                    {popupNames.leather
+                        ? <span><span>Leather:</span> <strong>{popupNames.leather}</strong></span>
+                        : null}
 
-            {popupNames.comfort && !popupNames.tableTopMaterial && !popupNames.materialOfTheLegs && !popupNames.material
-                ? <span><span>Comfort:</span> <strong>{popupNames.comfort}</strong></span>
-                : null}
+                    {popupNames.fabric
+                        ? <span><span>Fabric:</span> <strong>{popupNames.fabric}</strong></span>
+                        : null}
 
-            {popupNames.cover && !popupNames.tableTopMaterial && !popupNames.materialOfTheLegs && !popupNames.material
-                ? <span><span>Cover:</span> <strong>{popupNames.cover}</strong></span>
-                : null}
+                    {popupNames.legs
+                        ? <span><span>Legs:</span> <strong>{popupNames.legs}</strong></span>
+                        : null}
 
-            {popupNames.armrests
-                ? <span><span>Armrests:</span> <strong>{popupNames.armrests}</strong></span>
-                : null}
+                    {popupNames.materialOfTheLegs
+                        ? <span><span>Material of the legs:</span> <strong>{popupNames.materialOfTheLegs}</strong></span>
+                        : null}
 
-            {popupNames.accessories
-                ? <span><span>Accessories:</span> <strong>{popupNames.accessories}</strong></span>
-                : null}
+                    {popupNames.comfort && !popupNames.tableTopMaterial && !popupNames.materialOfTheLegs && !popupNames.material
+                        ? <span><span>Comfort:</span> <strong>{popupNames.comfort}</strong></span>
+                        : null}
 
-        </Data>
-    </Item>
-)
+                    {popupNames.cover && !popupNames.tableTopMaterial && !popupNames.materialOfTheLegs && !popupNames.material
+                        ? <span><span>Cover:</span> <strong>{popupNames.cover}</strong></span>
+                        : null}
+
+                    {popupNames.armrests
+                        ? <span><span>Armrests:</span> <strong>{popupNames.armrests}</strong></span>
+                        : null}
+
+                    {popupNames.accessories
+                        ? <span><span>Accessories:</span> <strong>{popupNames.accessories}</strong></span>
+                        : null}
+                </Data>
+                <button onClick={() => {navigator.clipboard.writeText(data.current.innerText); toast(copySuccessed['en'])}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24.762" height="30.471" viewBox="0 0 24.762 30.471">
+                        <g id="Group_777" data-name="Group 777" transform="translate(-1637 -2205.529)">
+                            <g id="Rectangle_98" data-name="Rectangle 98" transform="translate(1637 2210)" fill="none" stroke="#707070" stroke-width="1.5">
+                                <rect width="20" height="26" stroke="none" />
+                                <rect x="0.75" y="0.75" width="18.5" height="24.5" fill="none" />
+                            </g>
+                            <path id="Path_57" data-name="Path 57" d="M3577.607,933.279h19.405V957.7" transform="translate(-1936 1273)" fill="none" stroke="#707070" stroke-width="1.5" />
+                        </g>
+                    </svg>
+
+                    <span className="underline">
+                        Copy Configuration
+                    </span>
+                </button>
+            </Flex>
+        </Item>
+    )
+}
+
+const Flex = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap: 30px;
+
+    @media (max-width: 375px) {
+        flex-direction: column;
+        gap: 0;
+    }
+
+    button{
+        height: fit-content;
+        margin-top: 20px;
+        min-width: max-content;
+        width: fit-content;
+        cursor: pointer;
+        display: grid;
+        grid-template-columns:auto 1fr ;
+        align-items: center;
+        grid-gap: clamp(10px, ${5 / 390 * 100}vw, 15px);
+        border: none;
+        background-color: transparent;
+        svg{
+            width: clamp(15px, ${15 / 390 * 100}vw, 25px);
+        }
+        span{
+            font-size: clamp(10px, ${10 / 390 * 100}vw, 18px);
+        }
+
+        &:hover{
+            span{
+                background-size: 100% 1px;
+            }
+        }
+    }
+`
 
 const Item = styled.div`
     position: relative;
