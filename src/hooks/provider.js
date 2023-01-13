446c59cdf5b3e17b6a15536d41e1f409a21fc77c
KryptonumDev <kryptonumstudio@gmail.com>
2023-01-13 09:02:31 +0100

    Split check null error

--- src/hooks/provider.js
+++ src/hooks/provider.js
@@ -1,49 +1,47 @@
-import React, { useState } from "react"
-import { getCookie } from "../helpers/coockie-manager";
+import React, { useState } from 'react'
+import { getCookie } from '../helpers/coockie-manager'
 
-export const myContext = React.createContext();
+export const myContext = React.createContext()
 
 const Provider = ({ children }) => {
-
-    const [currentAlternates, changeAlternates] = useState(null)
-    const [searchInputValue, setSearchInputValue] = useState('')
-    const [favouritesCount, setFavouritesCount] = useState(() => {
-
-        let itemsCount = 0
-        if(typeof window !== 'undefined'){
-            itemsCount += getCookie('collections').split("|").length - 1
-            itemsCount += getCookie('products').split("|").length - 1
-            itemsCount += getCookie('materials').split("|").length - 1
-            itemsCount += getCookie('colors').split("|").length - 1
-        }
-
-        return itemsCount
-    })
-
-    const recalculateFavouritesCount = () => {
-        let itemsCount = 0
-
-        itemsCount += getCookie('collections').split("|").length - 1
-        itemsCount += getCookie('products').split("|").length - 1
-        itemsCount += getCookie('materials').split("|").length - 1
-        itemsCount += getCookie('colors').split("|").length - 1
-
-        setFavouritesCount(itemsCount)
+  const [currentAlternates, changeAlternates] = useState(null)
+  const [searchInputValue, setSearchInputValue] = useState('')
+  const [favouritesCount, setFavouritesCount] = useState(() => {
+    let itemsCount = 0
+    if (typeof window !== 'undefined') {
+      itemsCount += getCookie('collections')?.split('|').length - 1
+      itemsCount += getCookie('products')?.split('|').length - 1
+      itemsCount += getCookie('materials')?.split('|').length - 1
+      itemsCount += getCookie('colors')?.split('|').length - 1
     }
 
-    return (
-        <myContext.Provider value={{
-            currentAlternates,
-            changeAlternates,
-            searchInputValue,
-            setSearchInputValue,
-            favouritesCount,
-            recalculateFavouritesCount
-        }}>
-            {children}
-        </myContext.Provider>
-    )
+    return itemsCount
+  })
 
+  const recalculateFavouritesCount = () => {
+    let itemsCount = 0
+
+    itemsCount += getCookie('collections')?.split('|').length - 1
+    itemsCount += getCookie('products')?.split('|').length - 1
+    itemsCount += getCookie('materials')?.split('|').length - 1
+    itemsCount += getCookie('colors')?.split('|').length - 1
+
+    setFavouritesCount(itemsCount)
+  }
+
+  return (
+    <myContext.Provider
+      value={{
+        currentAlternates,
+        changeAlternates,
+        searchInputValue,
+        setSearchInputValue,
+        favouritesCount,
+        recalculateFavouritesCount
+      }}>
+      {children}
+    </myContext.Provider>
+  )
 }
 
 export default Provider
