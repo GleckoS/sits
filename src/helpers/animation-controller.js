b28efc7bcecd511f81e87d1a3e10a22e00e3e71c
kryptonumstudio <kryptonumstudio@gmail.com>
2023-04-24 19:20:11 +0200

    Revert "test disable animation"
    This reverts commit 4b211e8eb5f9ce55804d2f11afd2b78a36fa0ece.

--- src/helpers/animation-controller.js
+++ src/helpers/animation-controller.js
@@ -8,15 +8,44 @@ const transition = (index = 1, timing, isLink = false) => {
 
 export const linkTransition = (index, timing = 'fast') => {
     return {
+        initial: {
+            opacity: 0,
+            backgroundSize: '0 1px'
+        },
+        animate: {
+            opacity: 1,
+            transition: transition(index, timing),
+            transitionEnd: {
+                backgroundSize: '80% 1px',
+                transition: transition('slow')
+            }
+        }
     }
 }
 
 export const textTransition = (index, timing = 'fast') => {
     return {
+        initial: {
+            opacity: 0
+        },
+        animate: {
+            opacity: 1,
+            transition: transition(index, timing)
+        }
     }
 }
 
 export const imageTransition = (index, timing = 'slow') => {
     return {
+        initial: {
+            opacity: 0
+        },
+        animate: {
+            opacity: 1,
+            transition: transition(index, timing)
+        },
+        exit :{
+            opacity: 0
+        }
     }
 }
