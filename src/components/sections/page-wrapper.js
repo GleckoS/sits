27515e6924558ad6e308329adcd29b33cc234b0c
kryptonumstudio <kryptonumstudio@gmail.com>
2023-04-24 18:55:38 +0200

    Revert "window check"
    This reverts commit 7cc618170c7669ab191954fc8d0bda0f457ba2f6.

--- src/components/sections/page-wrapper.js
+++ src/components/sections/page-wrapper.js
@@ -9,22 +9,14 @@ export default function Wrapper({ children }) {
                 <motion.main
                     onAnimationStart={({ opacity }) => { setTransition(opacity) }}
                     onAnimationComplete={() => { document.body.classList.remove('loading') }}
-                    initial={{
-                        opacity: () => {
-                            if (typeof window === 'undefined') return 0
-                            return (document.body.classList.contains('loading') && window.innerWidth <= 640) ? 1 : 0
-                        }
-                    }}
+                    initial={{ opacity: (document.body.classList.contains('loading') && window.innerWidth <= 640) ? 1 : 0 }}
                     animate={{
                         opacity: 1,
                         transition: {
                             type: "spring",
                             mass: 0.35,
                             stiffness: 75,
-                            duration: () => {
-                                if (typeof window === 'undefined') return .2
-                                return (document.body.classList.contains('loading') && window.innerWidth <= 640) ? 0 : .2
-                            },
+                            duration: (document.body.classList.contains('loading') && window.innerWidth <= 640) ? 0 : .2,
                             when: 'beforeChildren'
                         }
                     }}
