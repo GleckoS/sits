c39fc09d323674b3ee972604d3741904493833ac
kryptonumstudio <kryptonumstudio@gmail.com>
2023-05-23 14:54:28 +0200

    fix underline

--- src/styles/global-style.js
+++ src/styles/global-style.js
@@ -129,8 +129,12 @@ export const Global = createGlobalStyle`
         background-position: left bottom;
         background-repeat: no-repeat;
 
+        @media (pointer: coarse) {
+            background-size: 80% 1px !important;
+        }
+
         &:hover {
-            background-size: 100% 1px;
+            background-size: 100% 1px !important;
         }
     }
     
