b44835e3bb18020a928408914d92bbcefbc4ae5b
kryptonumstudio <kryptonumstudio@gmail.com>
2023-09-14 10:45:22 +0200

    multilang 404

--- gatsby-node.js
+++ gatsby-node.js
@@ -850,3 +850,17 @@ exports.createPages = async ({
     })
 
 }
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage, deletePage } = actions
+
+  if (page.path.match(/^\/[a-z]{2}\/404\/$/)) {
+    const oldPage = { ...page }
+
+    const langCode = page.path.split(`/`)[1]
+    page.matchPath = `/${langCode}/*`
+
+    deletePage(oldPage)
+    createPage(page)
+  }
+}
